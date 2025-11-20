f49ab8ce5d16ab90301a267b9cff4e58e4cad6ff
jeetu <jitendradew2003@gmail.com>
2025-11-20 22:51:49 +0530

    Remove SST

--- src/routes/sst.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { generateCaptions } from "../controllers/sst.controller";
-
-const router = Router();
-router.post("/", generateCaptions);
-
-export default router;
